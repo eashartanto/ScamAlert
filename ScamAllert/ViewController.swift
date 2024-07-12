1ed45411695f45bcd9bb927a25d7f059124dd8d2
heimlichit <eashartanto@outlook.com>
2024-07-12 08:57:59 -0400

    Initial Commit

---
+++ ScamAllert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScamAllert
+//
+//  Created by Andreas Hartanto on 2024-07-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
