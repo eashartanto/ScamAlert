7985eb773aaf7ecf7df565638a75066364f72721
heimlichit <eashartanto@outlook.com>
2024-07-13 11:56:28 -0400

    adding article table view and create new article

---
+++ ScamAllert/CreateTabController.swift
@@ -0,0 +1,8 @@
+//
+//  CreateTabController.swift
+//  ScamAllert
+//
+//  Created by Andreas Hartanto on 2024-07-13.
+//
+
+import Foundation
---
+++ ScamAllert/HomeTabController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeTabController.swift
+//  ScamAllert
+//
+//  Created by Andreas Hartanto on 2024-07-12.
+//
+
+import Foundation
