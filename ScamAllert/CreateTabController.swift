75fc1451f3544ad808fbf4ce3492937ee7ceec16
heimlichit <eashartanto@outlook.com>
2024-07-13 11:59:06 -0400

    adding article table view and create new article commit

--- ScamAllert/CreateTabController.swift
+++ ScamAllert/CreateTabController.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+class CreateTabController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var detailField: UITextField!
+    @IBOutlet var phoneNumField: UITextField!
+    @IBOutlet var emailField: UITextField!
+    
+    public var completion: ((String, String) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+    }
+    
+
+    @objc func didTapSave() {
+        if let titleText = titleField.text, !titleText.isEmpty,
+                   let detailText = detailField.text, !detailText.isEmpty {
+                    completion?(titleText, detailText)
+        }
+    }
+
+
+}
--- ScamAllert/HomeTabController.swift
+++ ScamAllert/HomeTabController.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import UIKit
+
+
+class HomeTabController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var label: UILabel!
+    
+    var models: [(title: String, article: String)] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        title = "Article"
+    }
+
+    @IBAction func didCreateNewArticle(){
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "new") as? CreateTabController else {
+            return
+        }
+//        vc.title = "New Article"
+//        navigationController?.pushViewController(vc, animated: true)
+     
+        tabBarController?.selectedIndex = 1
+    }
+    //table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].article
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // show note controller
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "article") as? ArticleViewController else{
+            return
+        }
+        vc.title = "Article"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
